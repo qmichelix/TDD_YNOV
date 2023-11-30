ae23fb5987340003a69b8fe926f00b29cf5c3461
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-11-30 15:21:45 +0100

    Update test_TestProp.py

--- TEST/test_TestProp.py
+++ TEST/test_TestProp.py
@@ -1,13 +1,10 @@
 from LIVRE.Book import Book
 from LIVRE.BookManager import BookManager
 import pytest
-from hypothesis import given, strategies as st
+from hypothesis import HealthCheck, given, strategies as st
 
-@pytest.fixture
-def book_manager():
-    return BookManager()
-
-@given(st.lists(st.builds(Book, title=st.text(min_size=1), author=st.text(min_size=1)), unique=True))
+@given(st.lists(st.builds(Book, title=st.text(min_size=1), author=st.text(min_size=1)), unique=True), 
+       settings=st.settings(suppress_health_check=[HealthCheck.function_scoped_fixture]))
 def test_book_list_contains_all_books(book_manager, books):
     for book in books:
         book_manager.add_book(book)
