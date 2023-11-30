788a3b9bd416bd8f20824ba27bdca19071299cfb
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-11-30 15:18:24 +0100

    Update test_TestProp.py

--- TEST/test_TestProp.py
+++ TEST/test_TestProp.py
@@ -1,20 +1,14 @@
 from LIVRE.Book import Book
 from LIVRE.BookManager import BookManager
 import pytest
-
+from hypothesis import given, strategies as st
 
 @pytest.fixture
 def book_manager():
     return BookManager()
 
-# Importation de la bibliothèque Hypothesis
-from hypothesis import given, strategies as st
-
-def test_book_list_contains_all_books(book_manager):
-    @given(st.lists(st.builds(Book, title=st.text(min_size=1), author=st.text(min_size=1)), unique=True))
-    def property_test(books):
-        for book in books:
-            book_manager.add_book(book)
-        assert sorted(book_manager.list_books(), key=lambda book: book.title) == sorted(books, key=lambda book: book.title)
-    property_test()
-
+@given(st.lists(st.builds(Book, title=st.text(min_size=1), author=st.text(min_size=1)), unique=True))
+def test_book_list_contains_all_books(book_manager, books):
+    for book in books:
+        book_manager.add_book(book)
+    assert sorted(book_manager.list_books(), key=lambda book: book.title) == sorted(books, key=lambda book: book.title)
