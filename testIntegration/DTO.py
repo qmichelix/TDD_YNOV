85a06feb4814d01de75f332f8332e30159329100
Quentin MICHELIX <quentin.michelix@thelio.fr>
2023-12-01 14:10:36 +0100

    commit

---
+++ testIntegration/DTO.py
@@ -0,0 +1,16 @@
+class BookDTO:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def to_dict(self):
+        """ Convertit l'objet DTO en dictionnaire pour faciliter la sérialisation JSON. """
+        return {
+            "title": self.title,
+            "author": self.author
+        }
+
+    @staticmethod
+    def from_dict(data):
+        """ Crée un objet DTO à partir d'un dictionnaire. """
+        return BookDTO(title=data["title"], author=data["author"])
