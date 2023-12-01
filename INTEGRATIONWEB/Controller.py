7cc9c887069ace5a3f0bf819c33daac6719767fe
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-01 16:34:15 +0100

    Update Controller.py

--- INTEGRATIONWEB/Controller.py
+++ INTEGRATIONWEB/Controller.py
@@ -1,20 +1,27 @@
-# INTEGRATIONWEB/Controller.py
-
 from flask import Flask, request, jsonify
 from INTEGRATIONWEB.DTO import BookDTO
-from INTEGRATIONWEB.BookService import BookService
 
 app = Flask(__name__)
-book_service = BookService()
+
+# Simulated database for demonstration
+books_db = []
 
 @app.route('/books', methods=['GET'])
 def get_books():
-    books = book_service.get_books()
-    return jsonify([book.to_dict() for book in books])
+    return jsonify([book.to_dict() for book in books_db])
 
 @app.route('/books', methods=['POST'])
 def add_book():
     book_data = request.json
-    book = BookDTO.from_dict(book_data)
-    book_service.add_book(book)
-    return jsonify(book.to_dict()), 201
+    # Vérifiez si toutes les données nécessaires sont présentes
+    if 'title' not in book_data or 'author' not in book_data:
+        return jsonify({"error": "Missing data"}), 400
+    try:
+        book = BookDTO.from_dict(book_data)
+        books_db.append(book)
+        return jsonify(book.to_dict()), 201
+    except KeyError as e:
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
