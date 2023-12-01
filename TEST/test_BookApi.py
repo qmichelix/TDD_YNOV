b4f8afcff4a697caa3d76817d7f627faefa3ea41
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-01 14:33:51 +0100

    Create test_BookApi.py

---
+++ TEST/test_BookApi.py
@@ -0,0 +1,27 @@
+import pytest
+from flask_testing import TestCase
+from Controller import app
+from DTO import BookDTO
+
+class TestBookApi(TestCase):
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def test_get_books(self):
+        response = self.client.get('/books')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json, list)
+
+    def test_add_book(self):
+        book = {"title": "Test Book", "author": "Test Author"}
+        response = self.client.post('/books', json=book)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json, book)
+
+        # Verify if the book is added
+        get_response = self.client.get('/books')
+        self.assertIn(book, get_response.json)
+
+if __name__ == '__main__':
+    pytest.main()
