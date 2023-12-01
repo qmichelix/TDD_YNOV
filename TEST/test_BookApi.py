c032b81b0d5642c13f192587c624a04423b64c5d
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-01 16:50:27 +0100

    Update test_BookApi.py

--- TEST/test_BookApi.py
+++ TEST/test_BookApi.py
@@ -1,29 +1,22 @@
 import pytest
 from flask.testing import FlaskClient
+from unittest.mock import patch
 from INTEGRATIONWEB.Controller import app
-from INTEGRATIONWEB.DTO import BookDTO
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
-def test_get_books(client):
-    response = client.get('/books')
-    assert response.status_code == 200
-    assert isinstance(response.json, list)
+def test_get_books_with_mock(client):
+    with patch('INTEGRATIONWEB.Controller.BookService') as mock_service:
+        mock_service.get_books.return_value = [BookDTO("Mock Book", "Mock Author")]
+        response = client.get('/books')
+        assert response.status_code == 200
+        assert response.json == [{"title": "Mock Book", "author": "Mock Author"}]
 
-def test_add_book(client):
-    book = {"title": "Test Book", "author": "Test Author"}
-    response = client.post('/books', json=book)
-    assert response.status_code == 201
-    assert response.json == book
-
-    # Verify if the book is added
-    get_response = client.get('/books')
-    assert book in get_response.json
-
-def test_error_handling(client):
-    # Test error handling for bad request
-    response = client.post('/books', json={"title": "Incomplete Data"})
-    assert response.status_code == 400
+def test_error_handling_with_mock(client):
+    with patch('INTEGRATIONWEB.Controller.BookService') as mock_service:
+        mock_service.add_book.side_effect = Exception("Error")
+        response = client.post('/books', json={"title": "Test Book", "author": "Test Author"})
+        assert response.status_code == 500  # or whatever your error handling logic is
