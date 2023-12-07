daeabf10a057112e4e95b8f72d96efa0db373ed0
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-07 14:46:35 +0100

    Update Controller.py

--- INTEGRATIONWEB/Controller.py
+++ INTEGRATIONWEB/Controller.py
@@ -3,24 +3,28 @@ from LIVRE.Book import Book
 from LIVRE.BookDAO import BookDAO
 
 app = Flask(__name__)
-book_service = BookService()
+book_dao = BookDAO()  # Assurez-vous que BookDAO est correctement initialisé
 
 @app.route('/books', methods=['GET'])
 def get_books():
-    books = BookDAO.get_all_books()
-    return jsonify([book.to_dict() for book in books])
+    try:
+        books = book_dao.get_all_books()
+        return jsonify([book.to_dict() for book in books]), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/books', methods=['POST'])
 def add_book():
     book_data = request.json
-    book = Book(title=book_data['title'], author=book_data['author'])
-    BookDAO.add_book(book)
-    return jsonify(book.to_dict()), 201
+    if 'title' not in book_data or 'author' not in book_data:
+        return jsonify({"error": "Missing title or author"}), 400
 
+    try:
+        book = Book(title=book_data['title'], author=book_data['author'])
+        book_dao.add_book(book)
+        return jsonify(book.to_dict()), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-
-
-
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
