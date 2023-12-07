70363f589165d3523189b344d0ec83fcfd98a7ed
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-07 17:21:27 +0100

    Update BookDAO.py

--- LIVRE/BookDAO.py
+++ LIVRE/BookDAO.py
@@ -1,10 +1,17 @@
+from LIVRE.Book import Book
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 class BookDAO:
-    def __init__(self, db):
-        self.db = db
+    def __init__(self, db_url):
+        self.engine = create_engine(db_url)
+        self.Session = sessionmaker(bind=self.engine)
 
     def add_book(self, book):
-        self.db.session.add(book)
-        self.db.session.commit()
+        session = self.Session()
+        session.add(book)
+        session.commit()
 
     def get_all_books(self):
-        return Book.query.all()
+        session = self.Session()
+        return session.query(Book).all()
