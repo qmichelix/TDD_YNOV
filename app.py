f20c049786a150b0cc4d1bdaa4fb37c81cfe3038
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-08 00:52:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from LIVRE.Book import db  # Importez db de votre fichier Book.py
+from LIVRE.Book import db  # Assurez-vous que ce chemin d'importation est correct
 
 app = Flask(__name__)
+
+# Assurez-vous que cette URI de base de données est correcte et correspond à votre configuration CI/CD
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://utilisateur:mot_de_passe@localhost/nom_de_votre_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+# Cette partie est nécessaire uniquement si vous souhaitez créer des tables lors du démarrage de l'application
 with app.app_context():
-    db.create_all()  # Crée les tables si elles n'existent pas
+    db.create_all()
 
 # Autres configurations et routes de votre application
-from flask import Flask
-from models import db
-from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db.init_app(app)
-
-flask db init
-flask db migrate -m "Initial migration."
-flask db upgrade
