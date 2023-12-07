f2e5d31cd49b6feea7f2546fb869ff8e6eb09c5c
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-07 14:28:26 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    author = db.Column(db.String(80), nullable=False)
