82e4ec9e071eb99046f87bbf6630cce5ac8a9fb8
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-08 00:53:22 +0100

    Update models.py

--- models.py
+++ models.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Book(db.Model):
+    __tablename__ = 'books'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     author = db.Column(db.String(80), nullable=False)
+
+    # Autres méthodes et configurations si nécessaire
