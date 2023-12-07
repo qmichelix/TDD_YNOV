314e8c45ccef355b4bea2ea721f4806996edbfb7
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-07 14:43:07 +0100

    Update Controller.py

--- INTEGRATIONWEB/Controller.py
+++ INTEGRATIONWEB/Controller.py
@@ -1,18 +1,26 @@
 from flask import Flask, request, jsonify
-from INTEGRATIONWEB.DTO import BookDTO
-from INTEGRATIONWEB.BookService import BookService
+from LIVRE.Book import Book
+from LIVRE.BookDAO import BookDAO
 
 app = Flask(__name__)
 book_service = BookService()
 
 @app.route('/books', methods=['GET'])
 def get_books():
-    books = book_service.get_books()
+    books = BookDAO.get_all_books()
     return jsonify([book.to_dict() for book in books])
 
 @app.route('/books', methods=['POST'])
 def add_book():
     book_data = request.json
-    book = BookDTO.from_dict(book_data)
-    book_service.add_book(book)
+    book = Book(title=book_data['title'], author=book_data['author'])
+    BookDAO.add_book(book)
     return jsonify(book.to_dict()), 201
+
+
+
+
+
+
+
+
