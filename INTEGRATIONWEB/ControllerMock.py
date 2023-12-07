ea68c939f9dd60f3fca836839b2c82b9340f7bbe
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-07 16:51:14 +0100

    Update ControllerMock.py

--- INTEGRATIONWEB/ControllerMock.py
+++ INTEGRATIONWEB/ControllerMock.py
@@ -1,16 +1,18 @@
-from LIVRE.BookDAO import BookDAO
+from flask import Flask, request, jsonify
+from INTEGRATIONWEB.DTO import BookDTO
+from INTEGRATIONWEB.BookService import BookService
 
 app = Flask(__name__)
 book_service = BookService()
 
 @app.route('/books', methods=['GET'])
 def get_books():
-    books = BookDAO.get_all_books()
+    books = book_service.get_books()
     return jsonify([book.to_dict() for book in books])
 
 @app.route('/books', methods=['POST'])
 def add_book():
     book_data = request.json
-    book = Book(title=book_data['title'], author=book_data['author'])
-    BookDAO.add_book(book)
+    book = BookDTO.from_dict(book_data)
+    book_service.add_book(book)
     return jsonify(book.to_dict()), 201
