5cbac0f1be69e9858e5980a4dff505eb2210582f
qmichelix <109591838+qmichelix@users.noreply.github.com>
2023-12-08 10:35:52 +0100

    Update test_add_book.py

--- tests_component/test_add_book.py
+++ tests_component/test_add_book.py
@@ -1,1 +1,15 @@
+from behave import given, when, then
+import requests
 
+@given('the system is ready to add books')
+def step_impl(context):
+    context.url = "http://localhost:5000/books"
+
+@when('I add a book with title "{title}" and author "{author}"')
+def step_impl(context, title, author):
+    response = requests.post(context.url, json={"title": title, "author": author})
+    context.response = response
+
+@then('the book should be added successfully')
+def step_impl(context):
+    assert context.response.status_code == 201
